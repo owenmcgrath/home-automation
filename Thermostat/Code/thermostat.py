abe0cf226b08050f5df3476423774213ac4ba5ec
owenmcgrath <owenmcgrath85@gmail.com>
2020-03-15 20:56:26 +0000

    Fixed various thermostat bugs Thermostat now fully operational minus networking

--- Thermostat/Code/thermostat.py
+++ Thermostat/Code/thermostat.py
@@ -1,3 +1,5 @@
+
+
 import board
 import digitalio
 from PIL import Image, ImageDraw, ImageFont
@@ -6,28 +8,12 @@ import Adafruit_DHT
 import RPi.GPIO as GPIO
 import time
 
-def UpdateOledDisplay(oled, setting, temp):
-	# Clear display.
-	oled.fill(0)
-	oled.show()
-
-	# Create blank image for drawing.
-	# Make sure to create image with mode '1' for 1-bit color.
-	image = Image.new('1', (oled.width, oled.height))
-
-	# Get drawing object to draw on image.
-	draw = ImageDraw.Draw(image)
-
-	# Draw a black background
-	draw.rectangle((0, 0, oled.width, oled.height), outline=0, fill=0)
-
-
-	# Load default font.
-	font = ImageFont.load_default()
-
-	# Draw Some Text
-	text = "Room Temp: " + str(temp) + " *F"
-	text2 = "Setting: " str(setting) + " *F"
+def UpdateOledDisplay(oled, font, draw, image, setting, temp):
+	# Clear the image
+	draw.rectangle((0, 0, oled.width, oled.height), outline = 0, fill = 0)
+	# Draw the text
+	text = "Room Temp: " + str(round(temp, 1)) + " *F"
+	text2 = "Setting: " + str(setting) + " *F"
 
 	(font_width, font_height) = font.getsize(text)
 	draw.text((oled.width//2 - font_width//2, 3*oled.height//4 - font_height//2),
@@ -43,7 +29,7 @@ def GetTemp(dhtSensor, dhtPin):
 	humidity, temperature = Adafruit_DHT.read_retry(dhtSensor, dhtPin)
 	if(temperature is None):
 		temperature = -1 #Theres an error likely with the wiring
-	return temperature
+	return 1.8 * temperature + 32
 
 
 
@@ -64,60 +50,69 @@ if __name__ == "__main__":
 	i2c = board.I2C()
 	oled = adafruit_ssd1306.SSD1306_I2C(WIDTH, HEIGHT, i2c, addr=0x3d, reset=oled_reset)
 
+	oled.fill(0)
+	oled.show()
+
+	image = Image.new('1', (oled.width,oled.height))
+	draw = ImageDraw.Draw(image)
+	draw.rectangle((0,0,oled.width,oled.height), outline = 0, fill = 0)
+	font = ImageFont.load_default()
+
 	"""
 	DHT22 CONFIG
 	"""
 	DHT_SENSOR = Adafruit_DHT.DHT22
-	DHT_PIN = 22
+	DHT_PIN = 23
 
 	"""
 	BUTTON CONFIG
 	"""
-	UP_BUTTON_PIN = 13
-	DOWN_BUTTON_PIN = 15
+	UP_BUTTON_PIN = 27
+	DOWN_BUTTON_PIN = 22
 
 	GPIO.setwarnings(False)
-	GPIO.setmode(GPIO.BOARD)
 	GPIO.setup(UP_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 	GPIO.setup(DOWN_BUTTON_PIN, GPIO.IN, pull_up_down=GPIO.PUD_DOWN)
 
 	"""
 	RELAY CONFIG
 	"""
-	RELAY_PIN = 11
-	GPIO.setup(11, GPIO.OUT)
+	RELAY_PIN = 17
+	GPIO.setup(RELAY_PIN, GPIO.OUT)
 
 	"""
 	RUN THE THERMOSTAT!
 	"""
 	currSetting = 75
-	currTemp = 0
+	currTemp = GetTemp(DHT_SENSOR, DHT_PIN)
 	lastDownButtonState = False
 	lastUpButtonState = False
 	lineOpen = False
+	firstRun = True
 	lastLoopTime = 0
+	lastSetTime = 0
 	while True:
-		
 		#get the button input and update the current thermostat setting based on that
-		upButtonState = GPIO.input(13) == GPIO.HIGH
-		downButtonState = GPIO.input(15) == GPIO.HIGH
+		upButtonState = GPIO.input(UP_BUTTON_PIN) == GPIO.HIGH
+		downButtonState = GPIO.input(DOWN_BUTTON_PIN) == GPIO.HIGH
+		timestamp = time.time()
 		if (not lastDownButtonState) and downButtonState:
-			currSetting++
-			UpdateOledDisplay(oled,setting, time)
+			currSetting = currSetting - 1
+			UpdateOledDisplay(oled, font, draw, image, currSetting, currTemp)
+			lastSetTime = timestamp
 		if (not lastUpButtonState) and  upButtonState:
-			currSetting--
-			UpdateOledDisplay(oled, setting, time)
+			currSetting = currSetting + 1
+			print(currSetting)
+			UpdateOledDisplay(oled, font, draw, image, currSetting, currTemp)
+			lastSetTime = timestamp
 		lastUpButtonState = upButtonState
 		lastDownButtonState = downButtonState
-
-		#every second update the relay state
-		if time.time() - lastLoopTime > 1:
-
+		noInput = (timestamp - lastSetTime) > 5
+		if (lineOpen != (currTemp < currSetting)) or firstRun:
+			lineOpen = currTemp < currSetting
+			GPIO.output(RELAY_PIN, GPIO.HIGH if lineOpen else GPIO.LOW)
+			firstRun = False
+		if (timestamp - lastLoopTime) > 30 and noInput:
+			lastLoopTime = timestamp
 			currTemp = GetTemp(DHT_SENSOR, DHT_PIN)
-			lineOpen = currTemp < setting
-
-			GPIO.output(GPIO.HIGH if lineOpen else GPIO.LOW)
-			UpdateOledDisplay(oled, currSetting, currTemp)
-			lastLoopTime = time.time()
-
-
+			UpdateOledDisplay(oled, font, draw, image, currSetting, currTemp)
